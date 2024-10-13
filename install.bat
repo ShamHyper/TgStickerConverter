69ade375fdf094dc0ac1c383eacdcc135145502d
ShamHyper <makar.shatc@gmail.com>
2024-10-13 21:17:31 +0300

    first commit

---
+++ install.bat
@@ -0,0 +1,37 @@
+@echo off
+
+git pull
+
+set PYTHON=python
+set VENV_DIR=%~dp0venv
+
+mkdir tmp 2>NUL
+
+%PYTHON% -m venv "%VENV_DIR%"
+if %ERRORLEVEL% neq 0 (
+    echo Unable to create venv
+    pause
+    exit /b
+)
+
+if not exist "%VENV_DIR%\Scripts\python.exe" (
+    echo Creating venv in directory %VENV_DIR%
+    %PYTHON% -m venv "%VENV_DIR%"
+    if %ERRORLEVEL% neq 0 (
+        echo Unable to create venv
+        pause
+        exit /b
+    )
+)
+
+set PYTHON=%VENV_DIR%\Scripts\python.exe
+
+call "%VENV_DIR%\Scripts\activate"
+
+echo Installing dependencies...
+%PYTHON% -m pip install pip==24.0
+%PYTHON% -m pip install -r requirements.txt
+
+%PYTHON% app.py %*
+
+pause
